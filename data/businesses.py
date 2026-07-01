292bc953f8366624ccd0a29ffa2c3075a11c44cc
amirneisani <amir.neisani@gmail.com>
2026-07-01 17:04:00 +0330

    Add files via upload

---
+++ data/businesses.py
@@ -0,0 +1,72 @@
+# =====================================
+# اطلاعات کسب‌وکارها
+# =====================================
+
+BUSINESSES = {
+
+    "👗 مزون سیمین": {
+
+        "id": "mezon",
+
+        "title": "👗 مزون سیمین",
+
+        "banner": "images/banners/mezon.png",
+
+        "text": "SIMIN_TEXT",
+
+        "phone": "09126652134",
+
+        "address": "مهرشهر",
+
+        "gallery": [
+            "images/gallery/mezon/1.jpg",
+            "images/gallery/mezon/2.jpg",
+            "images/gallery/mezon/3.jpg"
+        ]
+
+    },
+
+    "👜 فروشگاه BERS": {
+
+        "id": "bers",
+
+        "title": "👜 فروشگاه BERS",
+
+        "banner": "images/banners/bers.png",
+
+        "text": "BERS_TEXT",
+
+        "phone": "09124672619",
+
+        "address": "جزیره کیش",
+
+        "gallery": [
+            "images/gallery/bers/1.jpg",
+            "images/gallery/bers/2.jpg",
+            "images/gallery/bers/3.jpg",
+            "images/gallery/bers/4.jpg"
+        ]
+
+    },
+
+
+    "🥩 پروتئینی یاشیل": {
+
+        "id": "yashil",
+
+  	  "title": "🥩 پروتئینی یاشیل",
+
+ 	   "banner": "images/banners/yashil.png",
+
+	    "text": "YASHIL_TEXT",
+
+	    "phone": "123456789",
+
+	    "address": "قم",
+
+	    "gallery": [
+    	    "images/gallery/yashil/1.jpg"
+ 	   ]
+
+	}
+}
