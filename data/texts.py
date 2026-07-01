d5bcf1e01adfdeec88c2e35ad1abb7b4053dbc6c
amirneisani <amir.neisani@gmail.com>
2026-07-01 19:48:36 +0330

    Update texts.py

--- data/texts.py
+++ data/texts.py
@@ -3,29 +3,29 @@
 # =====================================
 
 LINK_TEXT = """
-...
+a
 """
 
 WORK_TEXT = """
-...
+b
 """
 
 INCOME_TEXT = """
-...
+c
 """
 
 CONTACT_TEXT = """
-...
+d
 """
 
 SIMIN_TEXT = """
-...
+e
 """
 
 BERS_TEXT = """
-...
+f
 """
 
 YASHIL_TEXT = """
-...
+g
 """
